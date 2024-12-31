5ef5b1ee96ed7d9faf815279405b4c73cde2300e
Gazi Ehsanul Haque <antik.edu@gmail.com>
2024-12-31 16:16:45 +0600

    main

--- src/components/Document.tsx
+++ src/components/Document.tsx
@@ -1,7 +1,10 @@
 'use client'
-import React,{FormEvent, useState, useTransition} from 'react'
+import React,{FormEvent, useEffect, useState, useTransition} from 'react'
 import { Input } from './ui/input';
 import { Button } from './ui/button';
+import { doc, updateDoc } from 'firebase/firestore';
+import { db } from '../../firebase';
+import { useDocumentData } from 'react-firebase-hooks/firestore';
 
 type TDocumentProps = {
     id: string;
@@ -10,11 +13,24 @@ type TDocumentProps = {
 const Document = ({id}: TDocumentProps) => {
     const [input , setInput] = useState("")
     const [isUpdating , startTransition] = useTransition()
+    const firebaseConnection = doc(db, "documents", id);
+    const [data, loading, error] = useDocumentData(firebaseConnection)
+
+    useEffect(()=>{
+        startTransition(async ()=>{
+            setInput(data?.title);
+        })
+    },[data])
+
     const updateTitle = (e: FormEvent) =>{
         e.preventDefault();
         if(input.trim())
         {   
-            
+            startTransition(async()=>{
+                await updateDoc( doc(db , "documents" , id) , {
+                    title: input
+                });
+            })
         }
     }
   return (
